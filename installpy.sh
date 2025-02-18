fe80297ad29f8aa1525c2f370e5c011219e9e05e
xc112lg <152837944+xc112lg@users.noreply.github.com>
2025-02-19 04:32:46 +0800

    e

---
+++ installpy.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+#
+# Stock kernel for LG Electronics msm8996 devices build script by jcadduono
+# -(heavily)modified by stendro
+#
+############################# BEFORE STARTING #############################
+#
+# download a working toolchain and extract it somewhere and configure this
+# file to point to the toolchain's root directory.
+#
+# once you've set up the config section how you like it, you can simply run
+# ./build.sh [VARIANT]
+#
+################################ VARIANTS ################################
+#
+# H850		= International (Global)
+#		LGH850   (LG G5)
+#
+# H830		= T-Mobile (US)
+#		LGH830   (LG G5)
+#
+# RS988		= Unlocked (US)
+#		LGRS988  (LG G5)
+#
+#   *************************
+#
+# H910		= AT&T (US)
+#		LGH910   (LG V20)
+#
+# H915		= Canada (CA)
+#		LGH915   (LG V20)
+#
+# H918		= T-Mobile (US)
+#		LGH918   (LG V20)
+#
+# US996		= US Cellular & Unlocked (US)
+#		LGUS996  (LG V20)
+#
+# US996D	= US Cellular & Unlocked (US)
+#		LGUS996  (LG V20) (Unlocked with Engineering Bootloader)
+#
+# VS995		= Verizon (US)
+#		LGVS995  (LG V20)
+#
+# H990DS	= International (Global)
+#		LGH990   (LG V20)
+#
+# H990TR	= Turkey (TR)
+#		LGH990   (LG V20)
+#
+# LS997		= Sprint (US)
+#		LGLS997  (LG V20)
+#
+# F800K/L/S	= Korea (KR)
+#		LGF800   (LG V20)
+#
+#   *************************
+#
+# H870		= International (Global)
+#		LGH870   (LG G6)
+#
+# US997		= US Cellular & Unlocked (US)
+#		US997    (LG G6)
+#
+# H872		= T-Mobile (US)
+#		LGH872   (LG G6)
+#
+################################# CONFIG #################################
+sudo apt update
+sudo apt install binutils-aarch64-linux-gnu
+# Assume build_all is not being used, will be automatically changed if it is
+
+
+sudo apt update
+sudo apt install python2
+sudo add-apt-repository universe
+sudo apt update
+sudo apt install python2
