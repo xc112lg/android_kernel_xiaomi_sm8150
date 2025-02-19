6afa163b6c87a68e6a081986e06474a1027130d1
xc112lg <152837944+xc112lg@users.noreply.github.com>
2025-02-19 19:46:33 +0800

    f

--- build.sh
+++ build.sh
@@ -72,10 +72,17 @@ sudo apt install binutils-aarch64-linux-gnu
 
 git clone https://gitlab.com/crdroidandroid/android_prebuilts_clang_host_linux-x86_clang-r547379 clang-crdroid --depth 1
 export PATH=$(pwd)/clang-crdroid/bin:$PATH
+export CLANG_TRIPLE=aarch64-linux-gnu-
 export CROSS_COMPILE=$(pwd)/clang-crdroid/bin/aarch64-linux-gnu-
 export CROSS_COMPILE_ARM32=$(pwd)/clang-crdroid/bin/arm-linux-gnueabi-
 
 
+clang --version
+clang --version
+clang --version
+clang --version
+clang --version
+clang --version
 
 
 
@@ -239,7 +246,7 @@ SETUP_BUILD() {
     if [ $SINGLEBUILD = "yes" ]; then
 	  #  make -C "$RDIR" O=$BDIR CROSS_COMPILE=$CROSS_COMPILE $COMMON_DEFCONFIG $BOARD_DEFCONFIG $DEVICE_DEFCONFIG $DEBUG_DEFCONFIG \
 		
-        make -C "$RDIR" O=$BDIR  ARCH=arm64 CROSS_COMPILE=$CROSS_COMPILE $COMMON_DEFCONFIG $DEBUG_DEFCONFIG $BOARD_DEFCONFIG  $DEVICE_DEFCONFIG
+        make -C "$RDIR" O=$BDIR  ARCH=arm64  $COMMON_DEFCONFIG $DEBUG_DEFCONFIG $BOARD_DEFCONFIG  $DEVICE_DEFCONFIG
         #ARCH=arm64 LLVM=1 LLVM_IAS=1 $COMMON_DEFCONFIG $BOARD_DEFCONFIG $DEVICE_DEFCONFIG $DEBUG_DEFCONFIG \
     else # build_all will send make output to a file
         make -C "$RDIR" O=$BDIR CROSS_COMPILE=$CROSS_COMPILE $COMMON_DEFCONFIG $BOARD_DEFCONFIG $DEVICE_DEFCONFIG $SWAN2000_DEFCONFIG &> zBuild_all.log \
@@ -251,8 +258,15 @@ BUILD_KERNEL() {
 	    echo -e $COLOR_G"Compiling kernel for ${DEVICE}..."$COLOR_N
 	    TIMESTAMP1=$(date +%s)
     if [ $SINGLEBUILD = "yes" ]; then
-        make -C "$RDIR" O=$BDIR -j"$THREADS" ARCH=arm64 CC=clang CLANG_TRIPLE=aarch64-linux-gnu- CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_ARM32=arm-linux-gnueabi- LLVM=1 LLVM_IAS=1; do
-		    
+        make -C "$RDIR" O=$BDIR -j"$THREADS" ARCH=arm64 \
+			CC=clang \
+			CROSS_COMPILE=aarch64-linux-gnu- \
+			CROSS_COMPILE_ARM32=arm-linux-gnueabi- \
+			CLANG_TRIPLE=aarch64-linux-gnu- \
+			LLVM=1 \
+    		LLVM_IAS=1
+
+		
     else # build_all will send compile logs to a file
 	    while ! make -C "$RDIR" O=$BDIR -j"$THREADS" &> zBuild_all.log; do
 		    read -rp "Build failed. Retry? " do_retry
