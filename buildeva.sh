c3a5be54c700812db73c12146fa97eaa5b056f97
xc112lg <152837944+xc112lg@users.noreply.github.com>
2025-02-19 20:10:09 +0800

    s

--- buildeva.sh
+++ buildeva.sh
@@ -240,7 +240,7 @@ SETUP_BUILD() {
 		|| echo -e $COLOR_R"Failed to reflect device!"
     if [ $SINGLEBUILD = "yes" ]; then
 	    #ARCH=arm64 scripts/kconfig/merge_config.sh 
-	    make -C "$RDIR" O=$BDIR CROSS_COMPILE=$CROSS_COMPILE $COMMON_DEFCONFIG $DEBUG_DEFCONFIG $BOARD_DEFCONFIG  $DEVICE_DEFCONFIG \
+	    make -C "$RDIR" O=$BDIR ARCH=arm64 CROSS_COMPILE=$CROSS_COMPILE $COMMON_DEFCONFIG $DEBUG_DEFCONFIG $BOARD_DEFCONFIG  $DEVICE_DEFCONFIG \
 		    || ABORT "Failed to set up the kernel build."
     else # build_all will send make output to a file
         make -C "$RDIR" O=$BDIR CROSS_COMPILE=$CROSS_COMPILE $COMMON_DEFCONFIG $BOARD_DEFCONFIG $DEVICE_DEFCONFIG $SWAN2000_DEFCONFIG &> zBuild_all.log \
