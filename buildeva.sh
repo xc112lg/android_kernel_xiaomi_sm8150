d3d5f53fd66ca9109e7486da674f86c2d83a30e7
xc112lg <152837944+xc112lg@users.noreply.github.com>
2025-02-19 08:19:51 +0800

    e

--- buildeva.sh
+++ buildeva.sh
@@ -68,10 +68,10 @@
 ################################# CONFIG #################################
 sudo apt update
 sudo apt install binutils-aarch64-linux-gnu
-
+sudo apt install kmod
 #sudo -H apt-get install bc python2 ccache binutils-aarch64-linux-gnu cpio
 
-sudo ln -s $(which python3) /usr/bin/python2
+#sudo ln -s $(which python3) /usr/bin/python2
 
 # Assume build_all is not being used, will be automatically changed if it is
 SINGLEBUILD="yes"
