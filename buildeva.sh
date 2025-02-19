9b6b1aef6767cdd15be810793ad74565df4e6429
xc112lg <152837944+xc112lg@users.noreply.github.com>
2025-02-19 11:04:17 +0800

    d

--- buildeva.sh
+++ buildeva.sh
@@ -274,7 +274,7 @@ INSTALL_MODULES() {
 	echo -e $COLOR_G"Installing kernel modules..."$COLOR_N
     if [ $SINGLEBUILD = "yes" ]; then
         make -C "$RDIR" O=$BDIR \
-	        INSTALL_MOD_PATH="." \
+	        INSTALL_MOD_PATH="$BDIR" \
 	        INSTALL_MOD_STRIP=1 \
 	        modules_install
     else # build_all will send module logs to a file
