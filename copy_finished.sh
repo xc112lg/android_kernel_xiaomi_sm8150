be58df9bef2a3a1e3654ef403db6e51855de6a6d
xc112lg <152837944+xc112lg@users.noreply.github.com>
2025-02-19 08:50:01 +0800

    s

--- copy_finished.sh
+++ copy_finished.sh
@@ -120,7 +120,7 @@ COPY_KERNEL() {
     if [ $SINGLEBUILD = "yes" ]; then
 	    echo "Copying kernel image..."
     fi
-	cp $KERN_DIR/Image.${COMP}-dtb $DDIR \
+	cp $KERN_DIR/dtbo.img $DDIR \
 		|| ABORT "Failed to copy kernel image"
 	if grep -q 'CONFIG_MODULES=y' $BDIR/.config; then
       if [ $SINGLEBUILD = "yes" ]; then
