b44939628ceadaa48ea0a98f577e276003892d40
xc112lg <152837944+xc112lg@users.noreply.github.com>
2025-02-19 14:38:59 +0800

    s

--- build1.sh
+++ build1.sh
@@ -1,332 +1,93 @@
 #!/bin/bash
-#
-# Stock kernel for LG Electronics msm8996 devices build script by jcadduono
-# -(heavily)modified by stendro
-#
-############################# BEFORE STARTING #############################
-#
-# download a working toolchain and extract it somewhere and configure this
-# file to point to the toolchain's root directory.
-#
-# once you've set up the config section how you like it, you can simply run
-# ./build.sh [VARIANT]
-#
-################################ VARIANTS ################################
-#
-# H850		= International (Global)
-#		LGH850   (LG G5)
-#
-# H830		= T-Mobile (US)
-#		LGH830   (LG G5)
-#
-# RS988		= Unlocked (US)
-#		LGRS988  (LG G5)
-#
-#   *************************
-#
-# H910		= AT&T (US)
-#		LGH910   (LG V20)
-#
-# H915		= Canada (CA)
-#		LGH915   (LG V20)
-#
-# H918		= T-Mobile (US)
-#		LGH918   (LG V20)
-#
-# US996		= US Cellular & Unlocked (US)
-#		LGUS996  (LG V20)
-#
-# US996D	= US Cellular & Unlocked (US)
-#		LGUS996  (LG V20) (Unlocked with Engineering Bootloader)
-#
-# VS995		= Verizon (US)
-#		LGVS995  (LG V20)
-#
-# H990DS	= International (Global)
-#		LGH990   (LG V20)
-#
-# H990TR	= Turkey (TR)
-#		LGH990   (LG V20)
-#
-# LS997		= Sprint (US)
-#		LGLS997  (LG V20)
-#
-# F800K/L/S	= Korea (KR)
-#		LGF800   (LG V20)
-#
-#   *************************
-#
-# H870		= International (Global)
-#		LGH870   (LG G6)
-#
-# US997		= US Cellular & Unlocked (US)
-#		US997    (LG G6)
-#
-# H872		= T-Mobile (US)
-#		LGH872   (LG G6)
-#
-################################# CONFIG #################################
+# Thanks to https://t.me/itsclhex for build script and source
+sudo apt update && sudo -H apt-get install bc python2 ccache binutils-aarch64-linux-gnu cpio
 
+kernel_dir="${PWD}"
+CCACHE=$(command -v ccache)
+objdir="${kernel_dir}/out"
+anykernel=${PWD}/workspace/Ubuntu-SSH/anykernel
+builddir="${kernel_dir}/build"
+ZIMAGE=$kernel_dir/out/arch/arm64/boot/Image
+kernel_name="AnymoreProject-Kernel-vayu"
+zip_name="$kernel_name-$(date +"%d%m%Y-%H%M").zip"
+TC_DIR=${PWD}/workspace/
+CLANG_DIR=${PWD}/workspace/Ubuntu-SSH/clang-19
+export CONFIG_FILE="vayu_defconfig"
+export ARCH="arm64"
+export KBUILD_BUILD_HOST=rizalbrambe
+export KBUILD_BUILD_USER=t.me
 
-git clone https://gitlab.com/crdroidandroid/android_prebuilts_clang_host_linux-x86_clang-r547379 clang-crdroid
-export PATH=$(pwd)/clang-crdroid/bin:$PATH
-export CLANG_TRIPLE=aarch64-linux-gnu-
-export CROSS_COMPILE=$(pwd)/clang-crdroid/bin/aarch64-linux-gnu-
-export CROSS_COMPILE_ARM32=$(pwd)/clang-crdroid/bin/arm-linux-gnueabi-
+export PATH="$CLANG_DIR/bin:$PATH"
 
-# Assume build_all is not being used, will be automatically changed if it is
-SINGLEBUILD="yes"
-
-# root directory of this kernel (this script's location)
-RDIR=$(pwd)
-
-# build dir
-BDIR=build
-
-# version file
-VFIL=VERSION
-
-# expand version
-VER=$(cat $RDIR/$VFIL)
-
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# ------------------------- BUILD CONFIG OPTIONS -------------------------
-#
-# "user"@"host"
-KBUSER=stendro_+_AShiningRay
-KBHOST=github
-
-# ccache: yes or no
-USE_CCACHE=no
-
-# select cpu threads
-THREADS=$(grep -c "processor" /proc/cpuinfo)
-
-# directory containing cross-compiler
-# a newer toolchain (gcc8+) is recommended due to changes made
-# to the kernel.
-GCC_COMP=$HOME/gcc-arm64/bin/aarch64-elf-
-# directory containing 32bit cross-compiler for CONFIG_COMPAT_VDSO
-GCC_COMP_32=$HOME/gcc-arm/bin/arm-eabi-
-
-# -------------------------------- END -----------------------------------
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#
-# compiler version
-# gnu gcc or newer arm (linaro) gcc
-if $(${GCC_COMP}gcc --version | grep -q '(GCC)') || 
-$(${GCC_COMP}gcc --version | grep -q '(Eva GCC)'); then
-	GCC_STRING=$(${GCC_COMP}gcc --version | head -n1 | cut -f2 -d')')
-	GCC_VER="GCC$GCC_STRING"
-else # old linaro gcc
-	GCC_VER="$(${GCC_COMP}gcc --version | head -n1 | cut -f1 -d')' | \
-	cut -f2 -d'(')"
-	
-	if $(echo $GCC_VER | grep -q '~dev'); then
-  		GCC_VER="$(echo $GCC_VER | cut -f1 -d'~')+"
-fi
-fi
-
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# --------------------------- RIGID PORTION ------------------------------
-#
-# color codes
-COLOR_N="\033[0m"
-COLOR_R="\033[0;31m"
-COLOR_G="\033[1;32m"
-COLOR_Y="\033[1;33m"
-COLOR_P="\033[1;35m"
-
-ABORT() {
-	echo -e $COLOR_R"Error: $*"
-	exit 1
-}
-
-export ARCH=arm64
-export KBUILD_BUILD_USER=$KBUSER
-export KBUILD_BUILD_HOST=$KBHOST
-export LOCALVERSION="-${VER}"
-
-
-# In case a model isn't passed as an argument, this block acts as a fallback
-MODEL_ARRAY=("H850" "H830" "RS988" "H870" "US997" "H872" "H910" "H918" "H990" "LS997" "US996" "US996D" "VS995")
-FALLBACK_GET_VARIANT() {
-	if [[ ${SELECTED_MODEL} = "" ]]; then
-		echo -e "List of available variants:"
-		echo -e "G5  -> [$COLOR_C H850, H830, RS988 $COLOR_N]"
-		echo -e "G6  -> [$COLOR_C H870, US997, H872 $COLOR_N]"
-		echo -e "V20 -> [$COLOR_C H910, H918, H990, LS997, US996, US996D (Dirtysanta), VS995 $COLOR_N]"
-		read -p "Please select your model:" DEVICE
-	fi
-
-	# This checks if the user's model is supported by the kernel.
-	if [[ " ${MODEL_ARRAY[*]} " != *" ${DEVICE} "* ]];	then
-		echo -e "${COLOR_R}Your model wasn't found. Please check for errors (such as lower-case).${COLOR_N} \n"
-		FALLBACK_GET_VARIANT
-	fi
-}
-
-# selected device
-[ "$1" ] && DEVICE=$1
-[ "$DEVICE" ] || FALLBACK_GET_VARIANT
-
-# Checks if the build_all script isn't being used
-if [ "$2" = "build_all" ]; then
-    SINGLEBUILD="no"
-else
-    SINGLEBUILD="yes"
-fi
-
-# link device name to lg config files
-COMMON_DEFCONFIG=vendor/sm8150-perf_defconfig
-
-if [ "$DEVICE" = "vayu" ]; then
-  DEVICE_DEFCONFIG=vendor/xiaomi/vayu.config
-  BOARD_DEFCONFIG=vendor/xiaomi/sm8150-common.config
-  DEBUG_DEFCONFIG=vendor/debugfs.config
-elif [ "$DEVICE" = "H830" ]; then
-  DEVICE_DEFCONFIG=vendor/lge/h830.config
-  BOARD_DEFCONFIG=vendor/lge/lge_alice_common.config
-else
-  ABORT "Invalid device '${DEVICE}' specified! Make sure to use upper-case."
-fi
-
-SWAN2000_DEFCONFIG=vendor/lge/swan2000.config
-
-# check for stuff
-[ -f "$RDIR/arch/$ARCH/configs/${COMMON_DEFCONFIG}" ] \
-	|| ABORT "$COMMON_DEFCONFIG not found in $ARCH configs!"
-
-[ -f "$RDIR/arch/$ARCH/configs/${BOARD_DEFCONFIG}" ] \
-	|| ABORT "$BOARD_DEFCONFIG not found in $ARCH configs!"
-
-[ -f "$RDIR/arch/$ARCH/configs/${DEVICE_DEFCONFIG}" ] \
-	|| ABORT "$DEVICE_DEFCONFIG not found in $ARCH configs!"
-
-[ -x "${GCC_COMP}gcc" ] \
-	|| ABORT "Cross-compiler not found at: ${GCC_COMP}gcc"
-
-[ -x "${GCC_COMP_32}gcc" ] \
-	|| echo -e $COLOR_R"32-bit compiler not found, required for COMPAT_VDSO (VDSO32)."
-
-if [ "$USE_CCACHE" = "yes" ]; then
-	command -v ccache >/dev/null 2>&1 \
-	|| ABORT "Do you have ccache installed?"
-fi
-
-if [ -f "$BDIR/DEVICE" ] && \
-	[ "$(cat $BDIR/DEVICE)" = "$DEVICE" ]; then
-	ASK_CLEAN=yes
-fi
-
-# build commands
-CLEAN_BUILD() {
-	echo -e $COLOR_G"Cleaning build folder..."$COLOR_N
-	rm -rf $BDIR && sleep 5
-}
-
-SETUP_BUILD() {
-	echo -e $COLOR_G"Creating kernel config..."$COLOR_N
-	mkdir -p $BDIR
-	echo "$DEVICE" > $BDIR/DEVICE \
-		|| echo -e $COLOR_R"Failed to reflect device!"
-    if [ $SINGLEBUILD = "yes" ]; then
-	  #  make -C "$RDIR" O=$BDIR CROSS_COMPILE=$CROSS_COMPILE $COMMON_DEFCONFIG $BOARD_DEFCONFIG $DEVICE_DEFCONFIG $DEBUG_DEFCONFIG \
-		make -C "$RDIR" O=$BDIR ARCH=arm64 CC=clang CLANG_TRIPLE=aarch64-linux-gnu- CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_ARM32=arm-linux-gnueabi- $COMMON_DEFCONFIG $BOARD_DEFCONFIG $DEVICE_DEFCONFIG $DEBUG_DEFCONFIG \
-			    || ABORT "Failed to set up the kernel build."
-    else # build_all will send make output to a file
-        make -C "$RDIR" O=$BDIR CROSS_COMPILE=$CROSS_COMPILE $COMMON_DEFCONFIG $BOARD_DEFCONFIG $DEVICE_DEFCONFIG $SWAN2000_DEFCONFIG &> zBuild_all.log \
-		    || ABORT "Failed to set up the kernel build."
+if ! [ -d "$CLANG_DIR" ]; then
+    echo "Toolchain not found! Cloning to $CLANG_DIR..."
+    if ! git clone -q --depth=1 --single-branch https://gitlab.com/crdroidandroid/android_prebuilts_clang_host_linux-x86_clang-r547379.git -b 15.0 $TC_DIR; then
+        echo "Cloning failed! Aborting..."
+        exit 1
     fi
-}
-
-BUILD_KERNEL() {
-	    echo -e $COLOR_G"Compiling kernel for ${DEVICE}..."$COLOR_N
-	    TIMESTAMP1=$(date +%s)
-    if [ $SINGLEBUILD = "yes" ]; then
-        while ! make -C "$RDIR" O=$BDIR -j"$THREADS"; do
-		    read -rp "Build failed. Retry? " do_retry
-		    case $do_retry in
-			    Y|y) continue ;;
-			    *) ABORT "Compilation aborted." ;;
-		    esac
-	    done
-    else # build_all will send compile logs to a file
-	    while ! make -C "$RDIR" O=$BDIR -j"$THREADS" &> zBuild_all.log; do
-		    read -rp "Build failed. Retry? " do_retry
-		    case $do_retry in
-			    Y|y) continue ;;
-			    *) ABORT "Compilation aborted." ;;
-		    esac
-	    done
-    fi
-	    TIMESTAMP2=$(date +%s)
-	    BSEC=$((TIMESTAMP2-TIMESTAMP1))
-	    BTIME=$(printf '%02dm:%02ds' $(($BSEC/60)) $(($BSEC%60)))
-}
-
-INSTALL_MODULES() {
-	grep -q 'CONFIG_MODULES=y' $BDIR/.config || return 0
-	echo -e $COLOR_G"Installing kernel modules..."$COLOR_N
-    if [ $SINGLEBUILD = "yes" ]; then
-        make -C "$RDIR" O=$BDIR \
-	        INSTALL_MOD_PATH="." \
-	        INSTALL_MOD_STRIP=1 \
-	        modules_install
-    else # build_all will send module logs to a file
-        make -C "$RDIR" O=$BDIR \
-            INSTALL_MOD_PATH="." \
-            INSTALL_MOD_STRIP=1 \
-            modules_install &> zBuild_all.log
-    fi
-	rm $BDIR/lib/modules/*/build $BDIR/lib/modules/*/source
-}
-
-PREPARE_NEXT() {
-	if grep -q 'CONFIG_KERNEL_LZ4=y' $BDIR/.config; then
-	  echo lz4 > $BDIR/COMPRESSION \
-		|| echo -e $COLOR_R"Failed to reflect compression method!"
-	else
-	  echo gz > $BDIR/COMPRESSION \
-		|| echo -e $COLOR_R"Failed to reflect compression method!"
-	fi
-	git log --oneline -50 > $BDIR/GITCOMMITS \
-		|| echo -e $COLOR_R"Failed to reflect commit log!"
-}
-
-cd "$RDIR" || ABORT "Failed to enter $RDIR!"
-echo -e $COLOR_G"Building ${DEVICE} ${VER}..."
-echo -e $COLOR_P"Using $GCC_VER..."
-if [ "$USE_CCACHE" = "yes" ]; then
-  echo -e $COLOR_P"Using CCACHE..."
 fi
 
-# ask before cleaning if device
-# is the same as previous build
-if [ $SINGLEBUILD = "yes" ]; then
-    if [ "$ASK_CLEAN" = "yes" ]; then
-      while true; do
-        echo -e $COLOR_Y
-        read -p "Same device as the last build. Do you wish to clean the build directory?" yn
-        echo -e $COLOR_N
-        case $yn in
-          [Yy]* ) CLEAN_BUILD && break ;;
-          [Nn]* ) break ;;
-          * ) echo -e $COLOR_R"Please answer 'y' or 'n'"$COLOR_N ;;
-        esac
-      done
+# Colors
+NC='\033[0m'
+RED='\033[0;31m'
+LRD='\033[1;31m'
+LGR='\033[1;32m'
+
+make_defconfig()
+{
+    START=$(date +"%s")
+    echo -e ${LGR} "########### Generating Defconfig ############${NC}"
+    make -s ARCH=${ARCH} O=${objdir} ${CONFIG_FILE} -j$(nproc --all)
+}
+compile()
+{
+    cd ${kernel_dir}
+    echo -e ${LGR} "######### Compiling kernel #########${NC}"
+    make -j$(nproc --all) \
+    O=out \
+    ARCH=${ARCH}\
+    CC="ccache clang" \
+    CLANG_TRIPLE="aarch64-linux-gnu-" \
+    CROSS_COMPILE="aarch64-linux-gnu-" \
+    CROSS_COMPILE_ARM32="arm-linux-gnueabi-" \
+    LLVM=1 \
+    LLVM_IAS=1
+}
+
+completion()
+{
+    cd ${objdir}
+    COMPILED_IMAGE=arch/arm64/boot/Image
+    COMPILED_DTBO=arch/arm64/boot/dtbo.img
+    if [[ -f ${COMPILED_IMAGE} && ${COMPILED_DTBO} ]]; then
+
+        git clone -q https://github.com/AnymoreProject/AnyKernel3.git -b master $anykernel
+
+        mv -f $ZIMAGE ${COMPILED_DTBO} $anykernel
+
+        cd $anykernel
+        find . -name "*.zip" -type f
+        find . -name "*.zip" -type f -delete
+        zip -r AnyKernel.zip *
+        mv AnyKernel.zip $zip_name
+        mv $anykernel/$zip_name /workspace/Ubuntu-SSH/$zip_name
+        rm -rf $anykernel
+        END=$(date +"%s")
+        DIFF=$(($END - $START))
+        curl --upload-file /workspace/Ubuntu-SSH/$zip_name https://transfer.sh; echo
+        rm /workspace/Ubuntu-SSH/$zip_name
+        echo -e ${LGR} "############################################"
+        echo -e ${LGR} "############# OkThisIsEpic!  ##############"
+        echo -e ${LGR} "############################################${NC}"
+        exit 0
     else
-    CLEAN_BUILD
+        echo -e ${RED} "############################################"
+        echo -e ${RED} "##         This Is Not Epic :'(           ##"
+        echo -e ${RED} "############################################${NC}"
+        exit 1
     fi
-else # Always clean build folder for next build on build_all
-    CLEAN_BUILD
-fi
-SETUP_BUILD
-BUILD_KERNEL
-INSTALL_MODULES
-PREPARE_NEXT
-echo -e $COLOR_G"Finished building ${DEVICE} ${VER} -- Kernel compilation took"$COLOR_R $BTIME
-
-if [ $SINGLEBUILD = "yes" ]; then
-    echo -e $COLOR_P"Run './copy_finished.sh' to create the flashable AnyKernel zip."
-fi
+}
+make_defconfig
+compile
+completion
+cd ${kernel_dir}
