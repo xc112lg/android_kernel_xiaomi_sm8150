9cf6ff97776749481a74fcd3758e7c6fba75f9b6
xc112lg <152837944+xc112lg@users.noreply.github.com>
2025-02-20 21:19:45 +0800

    s

--- build.sh
+++ build.sh
@@ -257,7 +257,7 @@ INSTALL_MODULES() {
 	echo -e $COLOR_G"Installing kernel modules..."$COLOR_N
     if [ $SINGLEBUILD = "yes" ]; then
         make -C "$RDIR" O=$BDIR \
-	        INSTALL_MOD_PATH="." \
+	        INSTALL_MOD_PATH="$(realpath "$BDIR")" \
 	        INSTALL_MOD_STRIP=1 \
 	        modules_install
     else # build_all will send module logs to a file
@@ -299,7 +299,7 @@ fi
 #CLEAN_BUILD
 SETUP_BUILD
 
-BUILD_KERNEL
+#BUILD_KERNEL
 INSTALL_MODULES
 PREPARE_NEXT
 echo -e $COLOR_G"Finished building ${DEVICE} ${VER} -- Kernel compilation took"$COLOR_R $BTIME
