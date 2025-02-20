bac2c49aa8aeda4ff1fe3a7891e0228eb4af4c79
xc112lg <152837944+xc112lg@users.noreply.github.com>
2025-02-20 08:19:03 +0800

    f

--- build.sh
+++ build.sh
@@ -225,7 +225,7 @@ BUILD_KERNEL() {
 		# 	CLANG_TRIPLE=aarch64-linux-gnu- \
 		# 	LLVM=1 \
     	# 	LLVM_IAS=1
-		make O=out ARCH=arm64 CC="ccache clang" \
+		make O=out ARCH=arm64 CC=clang \
 			CLANG_TRIPLE="aarch64-linux-gnu-" \
 			CROSS_COMPILE="aarch64-linux-gnu-" \
 			CROSS_COMPILE_ARM32="arm-linux-gnueabi-" \
